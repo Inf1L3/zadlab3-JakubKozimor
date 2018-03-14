35f27b7c9c8ff3992f7b4587aca50d6eac34ffb5
gh <jyu>
2018-03-14 17:36:30 +0100

    zadania

--- POLab3/src/main/java/pl/edu/ur/POLab3/Main.java
+++ POLab3/src/main/java/pl/edu/ur/POLab3/Main.java
@@ -5,6 +5,8 @@
  */
 package pl.edu.ur.POLab3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Asia
@@ -14,10 +16,52 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO 1.4, 1.7, 
-        // TODO 2.2
-        // TODO 3.3, 3.4
+    public static int silnia(int wartosc) {
+        if (wartosc == 1) {
+            return 1;
+        } else {
+            return wartosc * silnia(wartosc - 1);
+        }
     }
-    
+
+    public static void main(String[] args) {
+/*
+        int a = 5;
+        int b = 3;
+        int c = a + b++;
+        int d = (a++) + b;
+        System.out.println(c);
+        System.out.println(d);
+        
+        int a = 6;
+        int b = 4;
+        int c = a & b;
+        int d = a | b;
+        System.out.println(c);
+        System.out.println(d);
+        a = 1;
+        b = 0;
+        if (a == 1 && b == 1) {
+            System.out.println("Tak");
+        } else {
+            System.out.println("Nie");
+
+        }
+        if (a == 1 || b == 1) {
+            System.out.println("Tak");
+        } else {
+            System.out.println("Nie");
+        }
+         */
+        Scanner pobierz = new Scanner(System.in);
+        int liczba = pobierz.nextInt();
+        System.out.println("Silnia= " + silnia(liczba));
+        int liczba1 = pobierz.nextInt();
+        int sil = 1;
+        for (int i = liczba1; i > 0; i--) {
+            sil = sil * i;
+        }
+        System.out.println("Silnia= " + sil);
+    }
+
 }
